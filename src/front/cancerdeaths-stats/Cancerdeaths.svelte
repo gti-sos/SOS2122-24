1e5bbf46af4316680fcdc3ffc7da4f057d909942
rgm13 <raulgomezm13@gmail.com>
2022-05-18 15:48:10 +0200

    prueba

--- src/front/cancerdeaths-stats/Cancerdeaths.svelte
+++ src/front/cancerdeaths-stats/Cancerdeaths.svelte
@@ -32,7 +32,7 @@
 
 	async function getcancerdeaths(){
 		console.log("fetching cancerdeaths ....");
-		let cadena = `/api/v1/cancerdeaths-stats?offset=+${offset}+&limit=+${limit}`;
+		let cadena = `/remoteAPI2`;
 		if(from !=null){
             cadena=cadena + `&from=${from}&` 
         }
